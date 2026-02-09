79fbf717147afeffe35a478a3186f3cff16090a8
FrancescoCaporale <frenk.caporale@gmail.com>
2026-02-09 17:54:51 +0100

    Modificata funzione show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,8 +5,19 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    res.send("Dettaglio del post " + req.params.id);
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+        return res.status(404).json({
+            error: "Not Found",
+            message: "Post non trovato"
+        });
+    }
+
+    res.json(post);
 }
+
 
 function store(req, res) {
     res.send("Creazione di un nuovo post");
